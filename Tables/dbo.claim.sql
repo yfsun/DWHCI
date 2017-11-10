f8c7778653b89fd8d87155cd4e96f2c8cff9e82e
unknown <bsun@CORP>
2017-11-10 14:05:43 -0500

    Added new Claims

--- Tables/dbo.claim.sql
+++ Tables/dbo.claim.sql
@@ -1,5 +1,6 @@
 CREATE TABLE [dbo].[claim]
 (
-[claimID] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[claimID] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[NewClaimID] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
